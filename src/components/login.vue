c11d41002ff81dff4571c8469c23d82e84ef9da0
NutchanonTangjai <6206021611109@fitm.kmutnb.ac.th>
2021-03-31 23:46:47 +0700

    login ยังไม่ส่งค่า

--- src/components/login.vue
+++ src/components/login.vue
@@ -7,7 +7,8 @@
             to="/"
             class="navbar-brand"
             style="display: block; margin-top: 15px; margin-left: 35.5px"
-            href="#">
+            href="#"
+          >
             <img src="../../img/logo.png" alt="" height="65" />
           </router-link>
         </div>
@@ -16,22 +17,92 @@
   </nav>
   <div class="bg">
     <div class="gradiant">
-        <div class="position-absolute top-50 start-50 translate-middle">
-            <form></form>
-        </div>
+      <div
+        class="position-absolute top-50 start-50 translate-middle"
+        style="
+          padding: 60px 68px 40px;
+          margin-bottom: 90px;
+          background: rgba(0, 0, 0, 0.75);
+          border-radius: 5px;
+          height: 550px;
+        "
+      >
+        <form>
+          <div>
+            <h1 class="h1">เข้าสู่ระบบ</h1>
+            <div style="padding: 0px 0px 16px">
+              <div class="form-floating">
+                <input
+                  v.model=""
+                  style="background: #333; border: 0"
+                  type="email"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                />
+                <label for="floatingInput" class="input"
+                  >อีเมลหรือหมายเลขโทรศัพท์</label
+                >
+              </div>
+            </div>
+            <div style="padding: 0px 0px 16px">
+              <div class="form-floating">
+                <input
+                  style="background: #333; border: 0"
+                  type="password"
+                  class="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                />
+                <label for="floatingPassword" class="input">รหัสผ่าน</label>
+              </div>
+            </div>
+            <div>
+              <button class="btn" style="color: white; font-weight: 900">
+                เข้าสู่ระบบ
+              </button>
+            </div>
+            <div style="margin-top:30px;color:#737373;">
+              หากยังใหม่กับการใช้ Dedphrik <router-link to="/" class="link">สมัครลงทะเบียน<br>ตอนนี้</router-link>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
-<style> 
+<style scoped>
 .gradiant {
   width: 100%;
-  height: 100vh;
   background: linear-gradient(
     0deg,
     rgba(0, 0, 0, 0.8) 8%,
     rgba(0, 0, 0, 0) 60%,
     rgba(0, 0, 0, 0.8) 94%
   );
+}
+.input {
+  color: #8c8c8c;
+}
+input:focus{
+  caret-color: white;
+}
+.link{
+  color: white;
+  text-decoration: none;
+}
+.link:hover{
+  color: white;
+  text-decoration: underline;
+}
+.btn {
+  margin-top: 24px;
+  margin-bottom: 12px;
+  border-radius: 4px;
+  width: 100%;
+  height: 48px;
+  border: 0;
+  background: #ff4f00;
 }
 .bg {
   width: 100%;
@@ -40,11 +111,26 @@
   background-size: cover;
   background-position: 50% 78%;
 }
+.form-floating {
+  width: 314px;
+  height: 50px;
+}
+.h1 {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 700;
+  margin-bottom: 28px;
+}
 </style>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      member:{
+        mail:"",
+        password:"",
+      }
+    };
   },
 };
 </script>
